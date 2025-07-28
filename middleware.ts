34c28834743697c05f384fa88ce2e445d391b688
michaelharrison1 <mharri97@lion.lmu.edu>
2025-07-28 17:20:26 -0500

    siteonline

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,3 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  return new NextResponse(
-    'Site is currently offline. Check back soon.',
-    { status: 503 }
-  )
-}
