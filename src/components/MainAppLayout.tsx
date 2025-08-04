a0341d43b28d2ff62b8690b420eb70ab3df7d4e0
michaelharrison1 <mharri97@lion.lmu.edu>
2025-08-04 16:50:52 -0400

    n

--- src/components/MainAppLayout.tsx
+++ src/components/MainAppLayout.tsx
@@ -79,7 +79,7 @@ const MainAppLayout: React.FC<MainAppLayoutProps> = ({
         minHeight: '100vh',
         minWidth: '100vw',
         width: '100%',
-        backgroundImage: 'url(/background.jpg)',
+        backgroundImage: 'url(/gifs/22.gif)',
         backgroundSize: 'cover',
         backgroundPosition: 'center center',
         backgroundRepeat: 'no-repeat',
